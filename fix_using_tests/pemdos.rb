c595d13b4483f4cafcb42c8c422290304ec71ab1
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-20 20:26:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,9 +4,9 @@ require 'pry'
 def snake_it_up(string)
 #   binding.pry
   if string.start_with?("s")
-  binding.pry
+#  binding.pry
   puts "ssssssssss" #+ "#{string}!"
-#binding.pry
+binding.pry
 
   else
     string
