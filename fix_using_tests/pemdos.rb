f0086088a81fba74d7746e12fb89f6da95150a41
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-20 20:34:59 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+#   binding.pry
+  if string.start_with?("s")
+#  binding.pry
+   "ssssssssss" + "#{string}"
+#binding.pry
+
   else
     string
   end
